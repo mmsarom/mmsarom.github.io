983c985c641df37cfb5b0fa250fb47f8c4b0d431
John Hor <horjohnm@gmail.com>
2024-06-03 01:38:42 +1000

    chore/install svelte gh pages plugin

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from 'svelte-adapter-github';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,11 +10,23 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter({
+		  // default options are shown. On some platforms
+		  // these options are set automatically — see below
+		  pages: 'docs',
+		  assets: 'docs',
+		  fallback: null,
+		  precompress: false,
+		  domain: '',
+		  jekyll: false
+		}),
+		paths: {
+			base: dev ? '' : '/msarom.github.io',
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal',
+	  }
 };
 
 export default config;
